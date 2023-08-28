7e3665794f573bff2ee20064c72ab685bab1a02a
StaticCtrl <StaticElite05@gmail.com>
2023-08-28 16:44:03 +0000

    Made into state machine
    Made the 7 else ifs into a switch with still 3 else ifs but oh well, still need to test it.

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@ int launch = 10;
 int nls = 11;
 int LaunchButton = 22;
 int Paddles = 23;
-int countup = 0;
-int countuprel = 1;
-int countdown = 0;
-int countdownrel = 1;
+int shift_state = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -27,91 +24,89 @@ void setup() {
 
 // Launch function
 void Launch() {
-  Serial.println("Launch");
   int LaunchValue = analogRead(LaunchButton);
-  // Set sensor to read Green only
   while (LaunchValue > 115 && LaunchValue < 126) {
     digitalWrite(clutch, HIGH);
     digitalWrite(launch, HIGH);
-    Serial.println("inside");
-    int LaunchValue = analogRead(LaunchButton);
-    if (LaunchValue > 145 && LaunchValue < 155) {
-      Serial.println("break");
-      break;
-    }
+    LaunchValue = analogRead(LaunchButton);
   }
+  // Serial.println("Go fast");
   digitalWrite(launch, LOW);
   delay(2000);
   digitalWrite(clutch, LOW);
 }
 
 void loop() {
+  delay(50);
+
+  // Read and print LaunchButton value & Paddles value
+  int LaunchValue = analogRead(LaunchButton);
+  // Serial.println(LaunchValue);
+  int PaddleValue = analogRead(Paddles);
+  // Serial.println(Paddles);
+
   // 152 is stock reading
   // 121 is the Launch Button
   // 112 is the Up Paddle
   // 101 is the Down Paddle
-  delay(50);
 
-  digitalWrite(led, HIGH);
+  switch (shift_state) {
+  // Read state
+  case 0:
+    if (PaddleValue < 145) {
+      shift_state = 1;
+    } else if (LaunchValue < 145) {
+      shift_state = 3;
+    } else {
+      shift_state = 4;
+    }
 
-  // Read and print LaunchButton value
-  int LaunchValue = analogRead(LaunchButton);
-  Serial.println(LaunchValue);
+  // Shift state
+  case 1:
+    if (PaddleValue > 108 && PaddleValue < 117) {
+      // Serial.println("Inside up case");
+      digitalWrite(nls, HIGH);
+      digitalWrite(up, HIGH);
+      delay(300);
+      shift_state = 2;
+    } else {
+      // Serial.println("Inside down case");
+      digitalWrite(clutch, HIGH);
+      digitalWrite(launch, HIGH);
+      delay(100);
+      digitalWrite(down, HIGH);
+      delay(100);
+      digitalWrite(clutch, LOW);
+      delay(100);
+      digitalWrite(launch, LOW);
+      shift_state = 2;
+    }
 
-  // Read and print Paddles value
-  int PaddleValue = analogRead(Paddles);
-  Serial.println(Paddles);
+  // Hold state
+  case 2:
+    while (PaddleValue < 145) {
+      // Serial.println("Holding shift");
+      delay(100);
+      shift_state = 2;
+    }
+    shift_state = 0;
 
-  // Up Shift (112)
-  if (PaddleValue > 108 && PaddleValue < 117 && countuprel > countup) {
-    // Serial.println("Inside up loop");
-    digitalWrite(nls, HIGH);
-    digitalWrite(up, HIGH);
-    delay(300);
-    countup++;
-  }
-
-  // Wait for release
-  else if (PaddleValue > 145 && countup == countuprel) {
-    // Serial.println("Hold up loop");
-    countuprel++;
-    Serial.println(countuprel);
-    delay(100);
-  }
-
-  // Down Shift (101)
-  else if (PaddleValue > 96 && PaddleValue < 106 && countdownrel > countdown) {
-    // Serial.println("Inside down loop");
-    digitalWrite(clutch, HIGH);
-    digitalWrite(launch, HIGH);
-    delay(100);
-    digitalWrite(down, HIGH);
-    delay(100);
-    digitalWrite(clutch, LOW);
-    delay(100);
-    digitalWrite(launch, LOW);
-    countdown++;
-  }
-
-  // Wait for release
-  else if (PaddleValue > 145 && countdown == countdownrel) {
-    // Serial.println("Hold down loop");
-    countdownrel++;
-    delay(100);
-  }
-
-  // LaunchControl
-  else if (LaunchValue > 115 && LaunchValue < 126) {
-    delay(1000);
+  // Launch state
+  case 3:
+    // Serial.println("Launch");
+    digitalWrite(led, HIGH);
     Launch();
-  }
+    digitalWrite(led, LOW);
+    shift_state = 0;
 
   // Default state
-  else {
+  case 4:
+    // Serial.println("Waiting for input");
     digitalWrite(down, LOW);
     digitalWrite(up, LOW);
     digitalWrite(nls, LOW);
     digitalWrite(clutch, LOW);
     digitalWrite(launch, LOW);
+    shift_state = 0;
   }
 }
