6c55dc4def4c60487fcd759a65872b79cc5b1218
Luis-Rodriguez2 <Luisrodriguez533700@gmail.com>
2025-02-17 21:29:03 -0500

    Updated code to be more readable

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,18 @@ int AcceptedPaddleValueUp = 1;
 int AcceptedPaddleValueDown = 1;
 int c = 0;
 
+//Reset for up shifts
+void upPinReset(){ 
+    digitalWrite(UP_PIN, LOW);
+    digitalWrite(NO_LIFT_SHIFT, LOW);
+}
+
+//Reset for down shifts
+void downPinReset(){
+    digitalWrite(DOWN_PIN, LOW);
+    digitalWrite(CLUTCH_PIN, LOW);
+}
+
 void setup(){
     pinMode(UP_PIN, OUTPUT);
     pinMode(NO_LIFT_SHIFT, OUTPUT);
@@ -31,50 +43,48 @@ void loop(){
     Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
     
     //Launch Control
-    while (Launch_Value = AcceptedLaunchValue /*Temp*/){
+    while (Launch_Value == AcceptedLaunchValue /*Temp*/){
         digitalWrite(CLUTCH_PIN, HIGH);
         Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL); /*Re-reads the button to update the signal and see if it is still pressed*/
     }
     digitalWrite(CLUTCH_PIN,LOW);
 
     //Paddle Shifting Up
-    while (Paddle_Value = AcceptedPaddleValueUp /*Temp*/){ 
+    while (Paddle_Value == AcceptedPaddleValueUp /*Temp*/){ 
         digitalWrite(NO_LIFT_SHIFT, HIGH);
         digitalWrite(UP_PIN, HIGH);
         int upSignal = analogRead(UP_SIGNAL);
         //Loop to wait for the cylinder to reach up
-        while (upSignal =! 1 /*Temp*/){
+        while (upSignal != 1 /*Temp*/){
             upSignal = analogRead(UP_SIGNAL);
             Serial.print("Waiting on pin to reach height");
             c = c + 1;
             //Safety Measure in case the sensor never reads the value and prevents infinite loop.
-            if (c = 100000 /*Temp, Value To Be Decided Later*/){
-                upSignal = 1 /*Temp, alternatively we could add a break here*/;
+            if (c == 100000 /*Temp, Value To Be Decided Later*/){
                 c = 0;
+                break;
             }
         }
-        //Reseting the values
-        digitalWrite(UP_PIN, LOW);
-        digitalWrite(NO_LIFT_SHIFT, LOW);
     }
+    upPinReset();
     
     //Paddle Shifting Down
-    while (Paddle_Value = AcceptedPaddleValueDown /*Temp*/){
+    while (Paddle_Value == AcceptedPaddleValueDown /*Temp*/){
         digitalWrite(CLUTCH_PIN, HIGH);
         digitalWrite(DOWN_PIN, HIGH);
         int downSignal = analogRead(DOWN_SIGNAL);
         //Loop to wait for the cylinder to reach up
-        while (downSignal =! 1 /*Temp*/){
+        while (downSignal != 1 /*Temp*/){
             downSignal = analogRead(DOWN_SIGNAL);
             Serial.print("Waiting on pin to reach height");
             c = c + 1;
             //Safety Measure in case the sensor never reads the value and prevents infinite loop.
             if (c = 100000 /*Temp, Value To Be Decided Later*/){
-                downSignal = 1 /*Temp, alternatively we could add a break here*/;
                 c = 0;
+                break;
             }
         }
-        digitalWrite(DOWN_PIN, LOW);
-        digitalWrite(CLUTCH_PIN, LOW);
     }
+    downPinReset(); //Reseting the down pins
+
 }
